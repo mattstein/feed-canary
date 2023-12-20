d7065a4d5ac5099dd2a571fb933073cf41f4a418
Matt Stein <m@ttste.in>
2023-12-20 13:40:16 -0800

    Don’t serialize those either.

--- app/Jobs/CheckFeed.php
+++ app/Jobs/CheckFeed.php
@@ -3,16 +3,14 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use App\Models\Feed;
 
 class CheckFeed implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable;
     public Feed $feed;
 
     /**
