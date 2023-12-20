47340befd5118da4dae80cf750f436ff9802614a
Matt Stein <m@ttste.in>
2023-12-20 10:47:39 -0800

    Maybe these settings need to be here?

--- config/mail.php
+++ config/mail.php
@@ -55,6 +55,12 @@ return [
             // 'client' => [
             //     'timeout' => 5,
             // ],
+
+            'domain' => env('MAILGUN_DOMAIN'),
+            'secret' => env('MAILGUN_SECRET'),
+            'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+            'scheme' => 'https',
+
         ],
 
         'postmark' => [
