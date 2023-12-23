36a5e19328cd048d86e2caa79279a4662b6f2687
Matt Stein <m@ttste.in>
2023-12-23 11:25:37 -0800

    Support JSON feeds, add console command for testing.

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use App\Mail\FeedFailed;
 use App\Mail\FeedFixed;
+use Fungku\MarkupValidator\FeedValidator;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
-use Fungku\MarkupValidator\FeedValidator;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
+use JsonSchema\Validator;
 
 /**
  * @property string $id
@@ -32,6 +33,9 @@ class Feed extends Model
     public const STATUS_HEALTHY = 'healthy';
     public const STATUS_FAILING = 'failing';
 
+    public const FORMAT_XML = 'xml';
+    public const FORMAT_JSON = 'json';
+
     public function manageUrl(): string
     {
         return url('/feed/' . $this->id);
@@ -44,6 +48,10 @@ class Feed extends Model
 
     public function validatorUrl(): string
     {
+        if ($this->getFormat() === self::FORMAT_JSON) {
+            return 'https://validator.jsonfeed.org/?url=' . urlencode($this->url);
+        }
+
         return 'https://validator.w3.org/feed/check.cgi?url=' . urlencode($this->url);
     }
 
@@ -60,8 +68,21 @@ class Feed extends Model
 
         if ($response->successful()) {
             Log::debug('Check successful');
-            $validator = new FeedValidator();
-            $isValid = $validator->validate($this->url);
+
+            if ($this->getFormat() === self::FORMAT_JSON) {
+                Log::debug('Validating JSON schema');
+                $schemaDefinition = resource_path('schema-v1.1.json');
+                $jsonSchemaObject = json_decode(file_get_contents($schemaDefinition));
+                $data = json_decode($response->getBody()->getContents());
+
+                $validator = new Validator;
+                $validator->validate($data, $jsonSchemaObject);
+                $isValid = $validator->isValid();
+            } else {
+                Log::debug('Validating XML schema');
+                $validator = new FeedValidator();
+                $isValid = $validator->validate($this->url);
+            }
         }
 
         Log::debug('Creating check record');
@@ -112,6 +133,15 @@ class Feed extends Model
             ->orderBy('updated_at', 'DESC')
             ->skip(1)
             ->first();
+    }
+
+    public function getFormat(): string
+    {
+        if (str_contains($this->type, 'application/json')) {
+            return self::FORMAT_JSON;
+        }
+
+        return self::FORMAT_XML;
     }
 
     private function statusHasChanged(): bool
