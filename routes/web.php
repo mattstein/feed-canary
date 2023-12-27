59a32c69ee9961a74b17898d95e64a9130eaef39
Matt Stein <m@ttste.in>
2023-12-27 14:27:43 -0800

    Add a quick status page.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
 use App\Http\Controllers\FeedController;
+use App\Models\Check;
 use App\Models\Feed;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+});
+
+Route::get('/status', function () {
+    $w3c = \Illuminate\Support\Facades\Http::get('https://validator.w3.org/feed');
+    $validatorDotOrg = \Illuminate\Support\Facades\Http::get('https://feedvalidator.org');
+
+    return view('status', [
+        'lastCheck' => Check::query()->orderBy('created_at', 'DESC')->first(),
+        'w3c' => $w3c->successful(),
+        'validatorDotOrg' => $validatorDotOrg->successful(),
+    ]);
 });
 
 Route::post('/add', [FeedController::class, 'create']);
