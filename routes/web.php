1d53c461edf1c7a137185e728d1354c2b9fff94b
Matt Stein <m@ttste.in>
2023-12-27 14:45:30 -0800

    Only update status every 30 seconds at most.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\FeedController;
 use App\Models\Check;
 use App\Models\Feed;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,13 +12,23 @@ Route::get('/', function () {
 });
 
 Route::get('/status', function () {
-    $w3c = \Illuminate\Support\Facades\Http::get('https://validator.w3.org/feed');
-    $validatorDotOrg = \Illuminate\Support\Facades\Http::get('https://feedvalidator.org');
+    $w3cUp = Cache::get('w3c-status');
+    $validatorDotOrgUp = Cache::get('validator.org-status');
+
+    if ($w3cUp === null) {
+        $w3cUp = Http::head('https://validator.w3.org/feed')->successful();
+        Cache::put('w3c-status', $w3cUp, 30);
+    }
+
+    if ($validatorDotOrgUp === null) {
+        $validatorDotOrgUp = Http::head('https://feedvalidator.org')->successful();
+        Cache::put('validator.org-status', $validatorDotOrgUp, 30);
+    }
 
     return view('status', [
         'lastCheck' => Check::query()->orderBy('created_at', 'DESC')->first(),
-        'w3c' => $w3c->successful(),
-        'validatorDotOrg' => $validatorDotOrg->successful(),
+        'w3c' => $w3cUp,
+        'validatorDotOrg' => $validatorDotOrgUp,
     ]);
 });
 
