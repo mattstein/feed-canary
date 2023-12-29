6d4d04e1045e92062dc10e9a3ebfe9e61357574e
Matt Stein <m@ttste.in>
2023-12-29 15:52:16 -0800

    Add a job for deleting checks > 30 days old.

---
+++ app/Jobs/PruneChecks.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Check;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+
+class PruneChecks implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $cutoff = Carbon::now()->subDays(30);
+        Check::query()
+            ->where('last_checked', '<=', $cutoff)
+            ->delete();
+    }
+}
