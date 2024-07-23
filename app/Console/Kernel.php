1d9f23903f6e9910b9d793be51bdfe8c13da991d
Matt Stein <m@ttste.in>
2024-07-23 13:57:16 -0700

    Fix scheduled command.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('backup:clean')->daily()->at('17:00');
         $schedule->command('backup:run')->daily()->at('17:30');
         $schedule->command('backup:monitor')->weekly()->at('18:00');
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +17,3 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Schedule::command('horizon:snapshot')->everyFiveMinutes();
