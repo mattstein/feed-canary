47beddf2989bdeca77b2de816a0c105e32310b52
Matt Stein <m@ttste.in>
2024-09-19 10:44:07 -0700

    Time out sooner and handle timeout exception.

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,26 @@ Route::get('/status', function () {
     $validatorDotOrgUp = Cache::get('validator.org-status');
 
     if ($w3cUp === null) {
-        $w3cUp = Http::head('https://validator.w3.org/feed')->successful();
+        try {
+            $w3cUp = Http::timeout(5)
+                ->head('https://validator.w3.org/feed')
+                ->successful();
+        } catch (\Illuminate\Http\Client\ConnectionException $e) {
+            $w3cUp = false;
+        }
+
         Cache::put('w3c-status', $w3cUp, 30);
     }
 
     if ($validatorDotOrgUp === null) {
-        $validatorDotOrgUp = Http::head('https://feedvalidator.org')->successful();
+        try {
+            $validatorDotOrgUp = Http::timeout(5)
+                ->head('https://www.feedvalidator.org')
+                ->successful();
+        } catch (\Illuminate\Http\Client\ConnectionException $e) {
+            $validatorDotOrgUp = false;
+        }
+        
         Cache::put('validator.org-status', $validatorDotOrgUp, 30);
     }
 
