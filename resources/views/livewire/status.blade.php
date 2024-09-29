adc76f2afa73abd693e01a7832db9ee9b2e3a198
Matt Stein <m@ttste.in>
2024-09-29 16:18:02 -0700

    Dodge unlikely error (and fix failing test).

--- resources/views/livewire/status.blade.php
+++ resources/views/livewire/status.blade.php
@@ -1,12 +1,17 @@
 <div class="container">
   <h1>Status</h1>
 
-  <p>Last check was <relative-time update="1"><time datetime="{{ $lastCheck->updated_at->format(DATE_ATOM) }}">{{ $lastCheck->updated_at }}</time></relative-time>.</p>
+  @if ($lastCheck)
+    <p>Last check was <relative-time update="1"><time datetime="{{ $lastCheck->updated_at->format(DATE_ATOM) }}">{{ $lastCheck->updated_at }}</time></relative-time>.</p>
+  @else
+    <p>Last check was ... never.</p>
+  @endif
 
-    <p wire:init="updateW3cStatus">
-      {{ $this->getStatusEmoji($w3cStatus) }} validator.w3.org/feed {{ $this->getStatusDescription($w3cStatus) }}
-    </p>
-    <p wire:init="updateValidatorDotOrgStatus">
-      {{ $this->getStatusEmoji($validatorDotOrgStatus) }} feedvalidator.org {{ $this->getStatusDescription($validatorDotOrgStatus) }}
-    </p>
+  <p wire:init="updateW3cStatus">
+    {{ $this->getStatusEmoji($w3cStatus) }} validator.w3.org/feed {{ $this->getStatusDescription($w3cStatus) }}
+  </p>
+
+  <p wire:init="updateValidatorDotOrgStatus">
+    {{ $this->getStatusEmoji($validatorDotOrgStatus) }} feedvalidator.org {{ $this->getStatusDescription($validatorDotOrgStatus) }}
+  </p>
 </div>
