ad7bf4402d0c4afe3894a83cbafd07542f90c24f
Matt Stein <m@ttste.in>
2024-09-29 14:19:53 -0700

    Remove now-obsolete route.

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -13,51 +13,6 @@ use Illuminate\Support\Str;
 
 class FeedController extends Controller
 {
-    public function create(Request $request)
-    {
-        $request->validate([
-            'url' => 'required|url',
-            'email' => 'required|email',
-        ]);
-
-        $feedUrl = $request->input('url');
-
-        try {
-            $response = Http::get($feedUrl);
-        } catch (ConnectionException $connectionException) {
-            $request->flash();
-
-            return Redirect::back()
-                ->withErrors([
-                    'url' => 'Couldn’t connect to that URL.',
-                ]);
-        }
-
-        $contentType = $response->header('content-type');
-
-        if (! Feed::isValidResponseType($contentType)) {
-            $request->flash();
-
-            return Redirect::back()
-                ->withErrors([
-                    'url' => 'That URL doesn’t return a JSON or RSS feed.',
-                ]);
-        }
-
-        $feed = Feed::create([
-            'url' => $feedUrl,
-            'email' => $request->input('email'),
-            'type' => $contentType,
-            'confirmation_code' => Str::random(),
-        ]);
-
-        $feed->check();
-
-        Mail::send(new ConfirmFeed($feed));
-
-        return redirect($feed->manageUrl());
-    }
-
     public function confirm(string $id, string $code)
     {
         $feed = Feed::where([
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class);
 Route::get('/status', Status::class);
-Route::post('/add', [FeedController::class, 'create']);
 Route::get('/feed/{id}', ManageFeed::class);
 Route::get('/feed/{id}/confirm/{code}', [FeedController::class, 'confirm']);
 Route::get('/updates', Updates::class);
