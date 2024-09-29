0bb8ce4d8793f250b69dc041ef1fc6f4bd1da27d
Matt Stein <m@ttste.in>
2024-09-29 14:46:22 -0700

    Tidy up.

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -2,14 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\ConfirmFeed;
 use App\Models\Feed;
-use Illuminate\Http\Client\ConnectionException;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Str;
 
 class FeedController extends Controller
 {
