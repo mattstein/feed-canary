98729d96d248b621373620729b1669895d162bac
Matt Stein <m@ttste.in>
2025-03-11 13:50:05 -0700

    Cleaner.

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+const originUrl = "https://feedcanary.ddev.site";
+const port = 3000;
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
     server: {
         host: "0.0.0.0",
-        port: 3000,
-        origin: 'https://feedcanary.ddev.site:3000',
+        port: port,
+        origin: `${originUrl}:${port}`,
         strictPort: true,
         cors: {
-            origin: 'https://feedcanary.ddev.site'
-        }
+            origin: originUrl,
+        },
     },
 });
