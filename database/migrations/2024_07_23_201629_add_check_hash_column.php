0a5ae0f041027854843306eecf2595caaf41b001
Matt Stein <m@ttste.in>
2025-05-05 13:15:39 -0700

    Add missing `down()` migration.

--- database/migrations/2024_07_23_201629_add_check_hash_column.php
+++ database/migrations/2024_07_23_201629_add_check_hash_column.php
@@ -21,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('checks', function (Blueprint $table) {
+            $table->dropColumn('hash');
+        });
     }
 };
