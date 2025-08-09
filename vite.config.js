46ffefa1751d7dc6adec49302431cafa8fc9012d
Matt Stein <m@ttste.in>
2025-08-09 16:34:41 -0700

    Use environment variables with fallbacks.

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 
-const originUrl = "https://feedcanary.ddev.site";
-const port = 3000;
-
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const originUrl = env.VITE_DEV_ORIGIN ?? "https://feedcanary.ddev.site";
+  const port = Number(env.VITE_DEV_PORT ?? 3000);
+  
+  return {
     plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
-        }),
+      laravel({
+        input: ["resources/css/app.css", "resources/js/app.js"],
+        refresh: true,
+      }),
     ],
     server: {
-        host: "0.0.0.0",
-        port: port,
-        origin: `${originUrl}:${port}`,
-        strictPort: true,
-        cors: {
-            origin: originUrl,
-        },
+      host: "0.0.0.0",
+      port,
+      origin: `${originUrl}:${port}`,
+      strictPort: true,
+      cors: {
+        origin: originUrl,
+      },
     },
+  };
 });
