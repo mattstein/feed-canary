a4a9d5d473830ef4faaddf3646baaac9fa8977cb
Matt Stein <m@ttste.in>
2026-03-12 15:48:06 -0700

    Drop B2 filesystem config.

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,15 +56,6 @@ return [
             'throw' => false,
         ],
 
-        'b2' => [
-            'driver' => 's3',
-            'key' => env('B2_APPLICATION_KEY_ID'),
-            'secret' => env('B2_APPLICATION_KEY_SECRET'),
-            'region' => env('B2_REGION', 'us-west-004'),
-            'bucket' => env('B2_BUCKET_NAME'),
-            'endpoint' => env('B2_ENDPOINT'),
-            'use_path_style_endpoint' => true,
-        ],
     ],
 
     /*
